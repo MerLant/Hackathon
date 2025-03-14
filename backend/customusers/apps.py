bbfa5f3eb230f8ae23519c050928330309f47a21
MerLant <m3rlant@gmail.com>
2025-03-15 02:01:17 +0500

    new format

--- backend/customusers/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CustomusersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'customusers'
-    verbose_name = 'Управление пользователями'
---
+++ detection/serializers.py
@@ -0,0 +1,10 @@
+import requests
+from rest_framework import serializers
+
+from .models import Image
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ('id', 'image', 'metadata')
