63b310965466ba78ab91ceb6e4c7eadb3b41aedb
MerLant <m3rlant@gmail.com>
2025-03-15 15:29:03 +0500

    fix

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -20,6 +20,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('detection.urls')),
